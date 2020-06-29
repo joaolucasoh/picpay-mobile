3172d1ca2d5daf678b6f9869875c8f4b8caed695
João Lucas Oliveira Hilario <iventis.joao@hubfintech.com.br>
2020-06-28 22:05:06 -0300

    Adjusting one method in android_screens file

--- features/step_definitions/happy_register_steps.rb
+++ features/step_definitions/happy_register_steps.rb
@@ -1,35 +1,34 @@
 # frozen_string_literal: true
 
 Given('that you perform the initial tutorial') do
-    @screen.tutorial.start_tutorial
-  end
-  
-  When('to start registration') do
-    @screen.register.register_screen?
-  end
-  
-  Then('I should see the fields email, phone number, password') do
-    @screen.register.fields?
-    @screen.register.happy_register
-  end
-  
-  Then('then send the valid code') do
-    @screen.register.send_token
-  end
-  
-  Then('I should see the {string} and the {string} section') do |categories, popular|
-    @screen.register.categories_page?(categories, popular)
-  end
-  
-  # skip code
-  
-  Then('not insert code and confirm') do
-    expect(@screen.skipcode.skip_message?).to be_truthy
-    @screen.skipcode.confirm_skip_code
-  end
-  
-  # login without user
-  When('to click on Skip') do
-    @screen.skipcode.skip_first
-  end
-  
+  @screen.tutorial.start_tutorial
+end
+
+When('to start registration') do
+  @screen.register.register_screen?
+end
+
+Then('I should see the fields email, phone number, password') do
+  @screen.register.fields?
+  @screen.register.happy_register
+end
+
+Then('then send the valid code') do
+  @screen.register.send_token
+end
+
+Then('I should see the {string} and the {string} section') do |categories, popular|
+  @screen.register.categories_page?(categories, popular)
+end
+
+# skip code
+
+Then('not insert code and confirm') do
+  expect(@screen.skipcode.skip_message?).to be_truthy
+  @screen.skipcode.confirm_skip_code
+end
+
+# login without user
+When('to click on Skip') do
+  @screen.skipcode.skip_first
+end
